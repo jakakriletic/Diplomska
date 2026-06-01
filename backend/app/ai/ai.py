ca2e6f35f2edc2d87ef0d4097da8e693b5d5e946
jakakriletic <jakakriletic1@gmail.com>
2026-06-01 22:31:05 +0200

    1.0.17

--- backend/app/ai/ai.py
+++ backend/app/ai/ai.py
@@ -1,40 +1,38 @@
-import os
-from google import genai
-from google.genai import types
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+
+load_dotenv()
 
 
-def generate():
-    client = genai.Client(
-        api_key=os.getenv("GEMINI_API_KEY"),
-    )
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str] = Field(default_factory=list)
+    tools_used: list[str] = Field(default_factory=list)
 
-    model = "gemini-3-flash-preview"
-    contents = [
-        types.Content(
-            role="user",
-            parts=[
-                types.Part.from_text(text="""HOW ARE YOU"""),
-            ],
-        ),
-    ]
-    tools = [
-        types.Tool(googleSearch=types.GoogleSearch(
-        )),
-    ]
-    generate_content_config = types.GenerateContentConfig(
-        thinking_config=types.ThinkingConfig(
-            thinking_level="HIGH",
-        ),
-        tools=tools,
-    )
 
-    for chunk in client.models.generate_content_stream(
-        model=model,
-        contents=contents,
-        config=generate_content_config,
-    ):
-        if text := chunk.text:
-            print(text, end="")
+llm = ChatOpenAI(
+    model="gpt-5.4-mini",
+    temperature=0
+)
 
-if __name__ == "__main__":
-    generate()
+agent = create_agent(
+    model=llm,
+    tools=[],
+    system_prompt=(
+        "You are a research assistant that helps generate research-style answers. "
+        "Return the answer in the required structured format."
+    ),
+    response_format=ResearchResponse,
+)
+
+result = agent.invoke({
+    "messages": [{"role": "user", "content": "WHos is the strongest armwrestler currently?"}]
+})
+
+try: 
+    print(result["structured_response"].model_dump())
+except Exception as e:
+    print("Error parsing response", e)
